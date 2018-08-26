3b5b348c68413dc1ba1ffacb17b8dacd16ee69eb
ravindra rawat <ravinrawat.wd@gmail.com>
2018-08-26 17:24:18 -0600

    test  create and find working

---
+++ test/reading_test.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Reading users from the database", () => {
+  let joe;
+  beforeEach(done => {
+    joe = new User({ name: "Joe" });
+    joe.save().then(() => done());
+  });
+
+  it("Finds all user with name joe ", done => {
+    User.find({ name: "Joe" }).then(users => {
+      assert(users[0]._id.toString() === joe._id.toString());
+      done();
+    });
+  });
+});
